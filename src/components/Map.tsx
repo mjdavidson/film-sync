caa5723b9de1a5e50c3900168a69458ca6a46338
Matt Davidson <matt@mattdavidson.kiwi>
2026-01-02 10:42:31 -0800

    add map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,22 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import type { LatLngExpression } from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
-function Map() {
+function Map({ coords }: { coords: LatLngExpression }) {
+  console.log({ coords });
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={coords}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{
+        height: "250px",
+      }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Marker position={coords}></Marker>
     </MapContainer>
   );
 }
--- src/components/MetadataInspector.tsx
+++ src/components/MetadataInspector.tsx
@@ -39,38 +39,66 @@ function MetadataInspector({
     [referenceFile],
   );
 
+  const gpsCoords = useMemo(() => {
+    if (
+      val?.GPSLatitude == null ||
+      val.GPSLongitude == null ||
+      val.GPSLatitudeRef == null ||
+      val.GPSLongitudeRef == null
+    ) {
+      return null;
+    }
+    const latDeg = val.GPSLatitude[0].toString();
+    const latMin = val.GPSLatitude[1].toString();
+    const latSec = val.GPSLatitude[2].toString();
+    const lngDeg = val.GPSLongitude[0].toString();
+    const lngMin = val.GPSLongitude[1].toString();
+    const lngSec = val.GPSLongitude[2].toString();
+
+    return `${latDeg}°${latMin}'${latSec}"${val.GPSLatitudeRef} ${lngDeg}°${lngMin}'${lngSec}"${val.GPSLongitudeRef}`;
+  }, [val]);
+
   if (referenceFile == null) {
     return null;
   }
 
   return (
-    <section className="bg-slate-50 p-6 rounded-xl border border-slate-100">
-      <img style={{ maxHeight: "100px" }} src={imgSrc} />
-      {val != null ? (
-        <>
-          <h3 className="font-semibold text-slate-900 mb-4">
-            Metadata Inspector
-          </h3>
-          <div className="space-y-4">
-            {val.CreateDate != null ? (
-              <MetadataRow
-                title="Originally created at"
-                value={formatISO(val.CreateDate)}
-              />
-            ) : null}
-            {val.GPSLatitude != null && val.GPSLongitude != null ? (
-              <div className="flex justify-between text-sm">
-                <span className="text-gray-500">GPS</span>
-                <span className="font-mono">
-                  {val.GPSLatitude} / {val.GPSLongitude}
-                </span>
+    <>
+      <section className="bg-slate-50 p-6 rounded-xl border border-slate-100 grid grid-cols-2 gap-4">
+        <div className="flex flex-col justify-center">
+          <img src={imgSrc} />
+        </div>
+        <div>
+          {val != null ? (
+            <>
+              <h3 className="font-semibold text-slate-900 mb-4">
+                Metadata Inspector
+              </h3>
+              <div className="space-y-4">
+                {val.CreateDate != null ? (
+                  <MetadataRow
+                    title="Originally created at"
+                    value={formatISO(val.CreateDate)}
+                  />
+                ) : null}
+                {gpsCoords != null ? (
+                  <MetadataRow title="GPS co-ords" value={gpsCoords} />
+                ) : null}
+                {val.latitude != null && val.longitude != null ? (
+                  <Map
+                    coords={{
+                      lat: val.latitude,
+                      lng: val.longitude,
+                      alt: val.GPSAltitude,
+                    }}
+                  />
+                ) : null}
               </div>
-            ) : null}
-          </div>
-        </>
-      ) : null}
-      <Map />
-    </section>
+            </>
+          ) : null}
+        </div>
+      </section>
+    </>
   );
 }
 
